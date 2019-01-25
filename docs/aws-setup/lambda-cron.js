813e734ef5135edaf47a40e5020db5d5e8ab3196
Omkar Manjrekar <manjrekarom@gmail.com>
2019-01-25 17:54:02 +0530

    Removed files causing main not found error in dev

--- docs/aws-setup/lambda-cron.js
+++ docs/aws-setup/lambda-cron.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
     var AWS = require('aws-sdk');
-    var iotdata = new AWS.IotData({endpoint: 'akbmorjah98q5.iot.ap-southeast-1.amazonaws.com'}); 
+    var iotdata = new AWS.IotData({endpoint: 'a221a6r4ojicsi-ats.iot.ap-southeast-1.amazonaws.com'});
     var params = { payload:JSON.stringify({"state": {"desired": JSON.parse(event.desired) } }), thingName:event.thingName };
     try {
         iotdata.updateThingShadow(params, function(err, data) {
---
+++ server/src/main/java/org/kyantra/resources/AssistantHookResource.java
@@ -0,0 +1,30 @@
+package org.kyantra.resources;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/assistant")
+public class AssistantHookResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String rePost(String jsonReq) throws UnirestException {
+        System.out.println(jsonReq);
+        HttpResponse<JsonNode> response = Unirest.post("http://127.0.0.1:8004/")
+                .header("Content-Type", "application/json")
+                .body(jsonReq)
+                .asJson();
+        System.out.println(response.getBody());
+        return String.valueOf(response.getBody());
+    }
+
+}
